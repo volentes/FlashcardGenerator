0f5106e38aba4afb7e85ad10fe5ea0f82ac5f3b9
Volente Stevens <Volente@Volentes-Air.attlocal.net>
2017-07-30 22:28:47 -0700

    final

--- ClozeCard.js
+++ ClozeCard.js
@@ -1,20 +1,18 @@
-//ClozeCard constructor taking parameter text and cloze
+//Taking 2 parameters
 var ClozeCard = function (text, cloze) {
-    //instance of operator tests if something is same object
-    //checking if this is clozeCard
+    //checkings for closecard
     if(this instanceof ClozeCard) {
-        //set fullText = to text bc that will equal entire text of the card
+        //set fullText = to text
         this.fullText = text;
-        //cloze is string we are removing from text and replacing with ... (answer)
+        //replaces with an answer
         this.cloze = cloze;
 
         //checking if text includes cloze which is true or false
         if(text.includes(cloze)) {
-            //true condition
-            //replacing the cloze portion of text with ...
+            //replacing the cloze portion 
             this.partial = text.replace(cloze, '...')
         } else {
-            //cloze not inside of text then error
+            //throws error if not found
             throw new Error(cloze + ' not found in ' + text);
         }
     } else {
